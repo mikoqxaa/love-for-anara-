bea4efe61add75b80414777d1c650e5bad23f7dd
mikoqxaa <shizalovemepls@gmail.com>
2025-03-22 01:00:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const heart = document.querySelector(".heart");
+
+    heart.addEventListener("click", function() {
+        alert("Анара, я тебя люблю!");
+    });
+});
