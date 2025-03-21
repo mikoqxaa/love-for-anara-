cd87c86d8811121cf85af109155422b0a9fed60c
mikoqxaa <shizalovemepls@gmail.com>
2025-03-22 01:09:44 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const heart = document.querySelector(".heart");
-
-    heart.addEventListener("click", function() {
-        alert("Анара, я тебя люблю!");
-    });
-});
